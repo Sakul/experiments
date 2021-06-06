1e51db44674d549f4d480fc17ae8892a43e94b86
Sakul <au.perspectives@gmail.com>
2021-06-06 15:45:00 +0700

    Demo AES (CCM, GCM)

--- xSecurity/ch6_symmetric_encryption/Program.cs
+++ xSecurity/ch6_symmetric_encryption/Program.cs
@@ -23,6 +23,22 @@ namespace ch6_symmetric_encryption
 
 			Console.WriteLine("\nAES");
 			EncryptAndDecrypt(plainText, new AesCryptoServiceProvider { Key = GetRandomData(32), IV = GetRandomData(16) });
+
+			var key = GetRandomData(32); // Key size (32 byte)
+			var nonce = GetRandomData(12); // Nonce size (12 byte)
+			var associatedData = GetRandomData(4); // Like PID, ReferenceId (it can be anything)
+
+			Console.WriteLine("\nAES (GCM)");
+			var encryptedGcm = EncryptGCM(plainText, key, nonce, associatedData);
+			var decryptedGcm = DecryptGCM(encryptedGcm.CipherText, key, nonce, encryptedGcm.Tag, associatedData);
+			Console.WriteLine($"Encrypted: {Convert.ToBase64String(encryptedGcm.CipherText)}");
+			Console.WriteLine($"Decrypted: {Encoding.ASCII.GetString(decryptedGcm)}");
+
+			Console.WriteLine("\nAES (CCM)");
+			var encryptedCcm = EncryptCCM(plainText, key, nonce, associatedData);
+			var decryptedCcm = DecryptCCM(encryptedCcm.CipherText, key, nonce, encryptedCcm.Tag, associatedData);
+			Console.WriteLine($"Encrypted: {Convert.ToBase64String(encryptedCcm.CipherText)}");
+			Console.WriteLine($"Decrypted: {Encoding.ASCII.GetString(decryptedCcm)}");
 		}
 
 		static byte[] GetRandomData(int size)
@@ -50,5 +66,43 @@ namespace ch6_symmetric_encryption
 			stream.FlushFinalBlock();
 			return memory.ToArray();
 		}
+
+		#region CCM
+		static (byte[] CipherText, byte[] Tag) EncryptCCM(string text, byte[] key, byte[] nonce, byte[] associatedData = null)
+		{
+			var byteText = Encoding.ASCII.GetBytes(text);
+			var tag = new byte[16];
+			var cipherText = new byte[byteText.Length];
+			using var gcm = new AesCcm(key);
+			gcm.Encrypt(nonce, byteText, cipherText, tag, associatedData);
+			return (cipherText, tag);
+		}
+		static byte[] DecryptCCM(byte[] cipherText, byte[] key, byte[] nonce, byte[] tag, byte[] associatedData = null)
+		{
+			var decryptedData = new byte[cipherText.Length];
+			using var gcm = new AesCcm(key);
+			gcm.Decrypt(nonce, cipherText, tag, decryptedData, associatedData);
+			return decryptedData;
+		}
+		#endregion CCM
+
+		#region GCM
+		static (byte[] CipherText, byte[] Tag) EncryptGCM(string text, byte[] key, byte[] nonce, byte[] associatedData = null)
+		{
+			var byteText = Encoding.ASCII.GetBytes(text);
+			var tag = new byte[16];
+			var cipherText = new byte[byteText.Length];
+			using var gcm = new AesGcm(key);
+			gcm.Encrypt(nonce, byteText, cipherText, tag, associatedData);
+			return (cipherText, tag);
+		}
+		static byte[] DecryptGCM(byte[] cipherText, byte[] key, byte[] nonce, byte[] tag, byte[] associatedData = null)
+		{
+			var decryptedData = new byte[cipherText.Length];
+			using var gcm = new AesGcm(key);
+			gcm.Decrypt(nonce, cipherText, tag, decryptedData, associatedData);
+			return decryptedData;
+		}
+		#endregion GCM
 	}
 }
